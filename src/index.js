c87d765b3e18543f970aa8de6280ff72fbfbc9b5
redhoodsu <redhoodsu@tencent.com>
2018-03-09 21:10:55 +0800

    Dev: Small changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import {loadCss, loadJs} from './util';
+
 module.exports = function (eruda) 
 {
     let {evalCss} = eruda.util;
@@ -12,10 +14,43 @@ module.exports = function (eruda)
         {
             super.init($el, container);
             $el.html(require('./template.hbs')());
+
+            this._initMap();
+            this._$info = this._$el.find('.eruda-info');
         }
         show() 
         {
             super.show();
+
+            this.resetView();
+        }
+        resetView() 
+        {
+            if (!navigator.geolocation) return;
+
+            navigator.geolocation.getCurrentPosition((position) =>
+            {  
+                var coords = position.coords,
+                    longitude = coords.longitude,
+                    latitude = coords.latitude;
+                    
+                this.setView(latitude, longitude);
+            }, (e) => 
+            {
+                this.setInfo(e.message);
+            }); 
+        }
+        setView(latitude, longitude) 
+        {
+            if (!this._map) return;
+            
+            this._map.setView([latitude, longitude], 12);
+
+            this.setInfo('latitude: ' + latitude + ' ' + 'longitude: ' + longitude);
+        }
+        setInfo(text) 
+        {
+            this._$info.text(text);
         }
         hide()
         {
@@ -25,6 +60,25 @@ module.exports = function (eruda)
         {
             super.destroy();
             evalCss.remove(this._style);
+        }
+        _initMap() 
+        {
+            loadCss('https://unpkg.com/leaflet@1.3.1/dist/leaflet.css');
+            loadJs('https://unpkg.com/leaflet@1.3.1/dist/leaflet.js', (isLoaded) =>
+            {
+                if (!isLoaded) return this.setInfo('Failed to init map');
+
+                this.setInfo('Map successfully initialized');
+
+                this._map = L.map('eruda-map').setView([39.9, 116.39], 12);
+                L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+                    maxZoom: 18,
+                    id: 'mapbox.streets',
+                    accessToken: 'pk.eyJ1Ijoic3VydW56aSIsImEiOiJjamVqbnk4c2gxN3JzMnltb3ByMXdkbDB5In0.Y6rCE361t15ATgiDb-o3Rw'
+                }).addTo(this._map);
+
+                this.resetView();
+            });
         }
     }
 
---
+++ src/util.js
@@ -0,0 +1,115 @@
+// Built by eustia.
+"use strict";
+
+var _ = {};
+
+/* ------------------------------ noop ------------------------------ */
+
+var noop = _.noop = (function ()
+{
+    /* A no-operation function.
+     *
+     * ```javascript
+     * noop(); // Does nothing
+     * ```
+     */
+
+    /* module
+     * env: all
+     * test: all
+     */
+
+    function exports() {}
+
+    return exports;
+})();
+
+/* ------------------------------ loadCss ------------------------------ */
+
+_.loadCss = (function ()
+{
+    /* Inject link tag into page with given href value.
+     *
+     * |Name|Type    |Desc           |
+     * |----|--------|---------------|
+     * |src |string  |Style source   |
+     * |cb  |function|Onload callback|
+     *
+     * ```javascript
+     * loadCss('style.css', function (isLoaded)
+     * {
+     *     // Do something...
+     * });
+     * ```
+     */
+
+    /* module
+     * env: browser
+     * test: browser
+     */
+
+    /* dependencies
+     * noop 
+     */ 
+
+    function exports(src, cb) 
+    {
+        cb = cb || noop;
+
+        var link = document.createElement('link');
+
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.onerror = function () { cb(false); }
+        link.onload = function () { cb(true); }
+        link.href = src;
+
+        document.head.appendChild(link);
+    }
+
+    return exports;
+})();
+
+/* ------------------------------ loadJs ------------------------------ */
+
+_.loadJs = (function ()
+{
+    /* Inject script tag into page with given src value.
+     *
+     * |Name|Type    |Desc           |
+     * |----|--------|---------------|
+     * |src |string  |Script source  |
+     * |cb  |function|Onload callback|
+     *
+     * ```javascript
+     * loadJs('main.js', function (isLoaded)
+     * {
+     *     // Do something...
+     * });
+     * ```
+     */
+
+    /* module
+     * env: browser
+     * test: browser
+     */
+
+    function exports(src, cb)
+    {
+        var script = document.createElement('script');
+        script.src = src;
+        script.onload = function ()
+        {
+            var isNotLoaded = script.readyState &&
+                script.readyState != 'complete' &&
+                script.readyState != 'loaded';
+
+            cb && cb(!isNotLoaded);
+        };
+        document.body.appendChild(script);
+    }
+
+    return exports;
+})();
+
+module.exports = _;
