d6c4778467caf1ed540fe854e57068cebf79ee50
surunzi <surunzi@foxmail.com>
2018-03-09 16:23:31 +0800

    Dev: Switch to webpack template

---
+++ src/index.js
@@ -0,0 +1,32 @@
+module.exports = function (eruda) 
+{
+    let {evalCss} = eruda.util;
+
+    class Geolocation extends eruda.Tool {
+        constructor() {
+            super();
+            this.name = 'geolocation';
+            this._style = evalCss(require('./style.scss'));
+        }
+        init($el, container) 
+        {
+            super.init($el, container);
+            $el.html(require('./template.hbs')());
+        }
+        show() 
+        {
+            super.show();
+        }
+        hide()
+        {
+            super.hide();
+        }
+        destroy() 
+        {
+            super.destroy();
+            evalCss.remove(this._style);
+        }
+    }
+
+    return new Geolocation();
+};
